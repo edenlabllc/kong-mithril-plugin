e529aa6c86e077cd563492c6b86bf7168970b55a
Alex Kovalevych <alexkovalevych@gmail.com>
2017-11-14 02:18:01 +0200

    [patch] set headers for subrequest

--- kong/plugins/mithril/handler.lua
+++ kong/plugins/mithril/handler.lua
@@ -16,6 +16,7 @@ function MithrilHandler:access(config)
     MithrilHandler.super.access(self)
 
     local authorization_header = ngx.req.get_headers()["authorization"]
+    local api_key =  ngx.req.get_headers()["api-key"]
 
     if authorization_header ~= nil then
         local bearer = string.sub(authorization_header, 8)
@@ -26,7 +27,8 @@ function MithrilHandler:access(config)
             method = "GET",
             headers = {
                 accept = "application/json",
-                ["Content-Type"] = "application/json"
+                ["Content-Type"] = "application/json",
+                ["api-key"] = api_key
             }
         })
 
@@ -40,6 +42,28 @@ function MithrilHandler:access(config)
             ngx.say(res.body)
             return ngx.exit(200)
         end
+
+        local response = json.decode(res.body)
+        local data = response.data or {}
+        local details = data.details or {}
+        local user_id = data.user_id or data.consumer_id
+        local scope = data.consumer_scope or details.scope
+
+        if user_id == nil or scope == nil then
+            ngx.status = 401
+            ngx.say("Invalid access token")
+            return ngx.exit(200)
+        end
+
+        ngx.req.set_header("x-consumer-id", user_id)
+        ngx.req.set_header("x-consumer-scope", scope)
+        if details.scope ~= nil then
+            ngx.req.set_header("x-consumer-metadata", json.encode(details))
+        end
+
+        -- 1. Add rules config
+        -- 2. Add custom validation of rules config (should be a valid json)
+        -- 3. Implement scope validation logic
     else
         ngx.status = 401
         ngx.header.content_type = "application/json"
