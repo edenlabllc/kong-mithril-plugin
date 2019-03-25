629392caca1fed612ccf162e49e7483b1b0f9ac0
Maryna <marinakrasnovatr81@gmail.com>
2019-03-25 17:49:26 +0200

    pr

--- kong/plugins/mithril/handler.lua
+++ kong/plugins/mithril/handler.lua
@@ -15,304 +15,304 @@ MithrilHandler.VERSION = "0.0.1"
 CorrelationIdHandler.PRIORITY = 1501
 
 local function send_error(status_code, message)
-    ngx.status = status_code
-    ngx.header.content_type = "application/json"
-    local type = "access_denied"
-    if status_code == 403 then
-        type = "forbidden"
-    end
+  ngx.status = status_code
+  ngx.header.content_type = "application/json"
+  local type = "access_denied"
+  if status_code == 403 then
+    type = "forbidden"
+  end
 
-    local port = ""
-    if ngx.var.server_port ~= 80 then
-        port = ":" .. ngx.var.server_port
-    end
-    local error = {
-        meta = {
-            url = ngx.var.scheme .. "://" .. ngx.var.host .. port .. ngx.var.request_uri,
-            type = "object",
-            request_id = ngx.ctx.correlationid_header_value,
-            code = status_code
-        },
-        error = {
-            type = type,
-            message = message
-        }
+  local port = ""
+  if ngx.var.server_port ~= 80 then
+    port = ":" .. ngx.var.server_port
+  end
+  local error = {
+    meta = {
+      url = ngx.var.scheme .. "://" .. ngx.var.host .. port .. ngx.var.request_uri,
+      type = "object",
+      request_id = ngx.ctx.correlationid_header_value,
+      code = status_code
+    },
+    error = {
+      type = type,
+      message = message
     }
-    ngx.say(json.encode(error))
+  }
+  ngx.say(json.encode(error))
 end
 
 local function validate_scopes(required_scopes, available_scopes)
-    local missing_scopes = {}
-    for k, required_scope in pairs(required_scopes) do
-        local has_scope = false
-        for key, consumer_scope in pairs(available_scopes) do
-            if required_scope == consumer_scope then
-                has_scope = true
-                break
-            end
-        end
-        if not has_scope then
-            table.insert(missing_scopes, required_scope)
-        end
+  local missing_scopes = {}
+  for k, required_scope in pairs(required_scopes) do
+    local has_scope = false
+    for key, consumer_scope in pairs(available_scopes) do
+      if required_scope == consumer_scope then
+        has_scope = true
+        break
+      end
     end
+    if not has_scope then
+      table.insert(missing_scopes, required_scope)
+    end
+  end
 
-    return missing_scopes
+  return missing_scopes
 end
 
 local function find_rule(rules)
-    local api_path = rstrip(ngx.ctx.router_matches.uri, "/")
-    local request_path = ngx.var.uri
+  local api_path = rstrip(ngx.ctx.router_matches.uri, "/")
+  local request_path = ngx.var.uri
 
-    local api_relative_path, n, err = ngx.re.gsub(request_path, api_path, "")
-    local method = ngx.req.get_method()
+  local api_relative_path, n, err = ngx.re.gsub(request_path, api_path, "")
+  local method = ngx.req.get_method()
 
-    for k, rule in pairs(rules) do
-        local path_matched, err = ngx.re.match(api_relative_path, "^" .. rule.path)
+  for k, rule in pairs(rules) do
+    local path_matched, err = ngx.re.match(api_relative_path, "^" .. rule.path)
 
-        method_matched = false
-        for key, rule_method in pairs(rule.methods) do
-            if rule_method == method then
-                method_matched = true
-                break
-            end
-        end
-        if path_matched and method_matched then
-            return rule
-        end
+    method_matched = false
+    for key, rule_method in pairs(rule.methods) do
+      if rule_method == method then
+        method_matched = true
+        break
+      end
     end
+    if path_matched and method_matched then
+      return rule
+    end
+  end
 end
 
 local function verify_url(error_msg)
-    local api_key = ngx.req.get_headers()["api-key"]
-    local httpc = http.new()
-    local res, err =
-        httpc:request_uri(
-            url,
-            {
-                method = "GET",
-                headers = {
-                    accept = "application/json",
-                    ["Content-Type"] = "application/json",
-                    ["api-key"] = api_key,
-                    ["x-request-id"] = ngx.ctx.correlationid_header_value
-                }
-            }
-        )
+  local api_key = ngx.req.get_headers()["api-key"]
+  local httpc = http.new()
+  local res, err =
+    httpc:request_uri(
+      url,
+      {
+        method = "GET",
+        headers = {
+          accept = "application/json",
+          ["Content-Type"] = "application/json",
+          ["api-key"] = api_key,
+          ["x-request-id"] = ngx.ctx.correlationid_header_value
+        }
+      }
+    )
 
-    httpc:close()
+  httpc:close()
 
-    if not verify_result or verify_result.status ~= 200 then
-        send_error(401, error_msg)
-        return ngx.exit(200), true
-    end
-    return res
+  if not verify_result or verify_result.status ~= 200 then
+    send_error(401, error_msg)
+    return ngx.exit(200), true
+  end
+  return res
 end
 
 local function verify_details()
-    local response = json.decode(verify_result.body)
-    local data = response.data or {}
-    local urgent = response.urgent or {}
-    local mis_client_id = urgent.mis_client_id
-    local details = data.details or {}
-    local broker_scope = details.broker_scope
-    local user_id = data.user_id or data.consumer_id
-    local scope = data.consumer_scope or details.scope
-    return mis_client_id, details, scope, broker_scope, user_id
+  local response = json.decode(verify_result.body)
+  local data = response.data or {}
+  local urgent = response.urgent or {}
+  local mis_client_id = urgent.mis_client_id
+  local details = data.details or {}
+  local broker_scope = details.broker_scope
+  local user_id = data.user_id or data.consumer_id
+  local scope = data.consumer_scope or details.scope
+  return mis_client_id, details, scope, broker_scope, user_id
 end
 
 function MithrilHandler:new()
-    MithrilHandler.super.new(self, "mithril")
+  MithrilHandler.super.new(self, "mithril")
 end
 
 function MithrilHandler:access(config)
-    MithrilHandler.super.access(self)
-    local cookie, err = ck:new()
+  MithrilHandler.super.access(self)
+  local cookie, err = ck:new()
 
-    if config.mis_only == true then
-        do_process_mis_only()
+  if config.mis_only == true then
+    do_process_mis_only()
+  else
+    local authorization
+    local field, err = cookie:get("authorization")
+    if not field then
+      authorization = ngx.req.get_headers()["authorization"]
     else
-        local authorization
-        local field, err = cookie:get("authorization")
-        if not field then
-            authorization = ngx.req.get_headers()["authorization"]
-        else
-            authorization = "Bearer " .. field
-        end
-
-        do_process(authorization)
+      authorization = "Bearer " .. field
     end
+
+    do_process(authorization)
+  end
 end
 
 local function set_mis_client_id(scope, mis_client_id, details)
-    ngx.req.set_header("x-consumer-scope", scope)
-    ngx.var.upstream_x_mis_client_id = mis_client_id
-    if details.scope ~= nil then
-        local x_consumer_metadata = json.encode(details)
-        ngx.req.set_header("x-consumer-metadata", x_consumer_metadata)
-        ngx.var.upstream_x_client_id = details.client_id
-    end
+  ngx.req.set_header("x-consumer-scope", scope)
+  ngx.var.upstream_x_mis_client_id = mis_client_id
+  if details.scope ~= nil then
+    local x_consumer_metadata = json.encode(details)
+    ngx.req.set_header("x-consumer-metadata", x_consumer_metadata)
+    ngx.var.upstream_x_client_id = details.client_id
+  end
 end
 
 
 local function check_scopes(rule, scope, broker_scope)
-    if rule == nil then
-        send_error(403, "ACL: No matching rule was found for path " .. ngx.ctx.router_matches.uri)
-        return ngx.exit(200)
-    end
+  if rule == nil then
+    send_error(403, "ACL: No matching rule was found for path " .. ngx.ctx.router_matches.uri)
+    return ngx.exit(200)
+  end
 
-    if scope == nil or scope == "" then
-        send_error(
-            403,
-            "Your scope does not allow to access this resource. Missing allowances: " .. table.concat(rule.scopes, ", ")
-        )
-        return ngx.exit(200)
-    end
+  if scope == nil or scope == "" then
+    send_error(
+      403,
+      "Your scope does not allow to access this resource. Missing allowances: " .. table.concat(rule.scopes, ", ")
+    )
+    return ngx.exit(200)
+  end
 
-    local missing_scopes = validate_scopes(rule.scopes, split(scope, " "))
+  local missing_scopes = validate_scopes(rule.scopes, split(scope, " "))
+  if #missing_scopes > 0 then
+    send_error(
+      403,
+      "Your scope does not allow to access this resource. Missing allowances: " ..
+      table.concat(missing_scopes, ", ")
+    )
+    return ngx.exit(200)
+  end
+
+  if broker_scope ~= nil then
+    local missing_scopes = validate_scopes(rule.scopes, split(broker_scope, " "))
     if #missing_scopes > 0 then
-        send_error(
-            403,
-            "Your scope does not allow to access this resource. Missing allowances: " ..
-            table.concat(missing_scopes, ", ")
-        )
-        return ngx.exit(200)
+      send_error(
+        403,
+        "Your scope does not allow to access this resource. Missing allowances: " ..
+        table.concat(missing_scopes, ", ")
+      )
+      return ngx.exit(200)
     end
-
-    if broker_scope ~= nil then
-        local missing_scopes = validate_scopes(rule.scopes, split(broker_scope, " "))
-        if #missing_scopes > 0 then
-            send_error(
-                403,
-                "Your scope does not allow to access this resource. Missing allowances: " ..
-                table.concat(missing_scopes, ", ")
-            )
-            return ngx.exit(200)
-        end
-    end
+  end
 end
 
 local function check_abac(rule, user_id, mis_client_id, details)
-    local abac = rule.abac
-    if abac then
-        local m, err = ngx.re.match(ngx.var.request_uri, abac.rule)
+  local abac = rule.abac
+  if abac then
+    local m, err = ngx.re.match(ngx.var.request_uri, abac.rule)
 
-        -- Rule was found
-        if m then
-            local resource_id = m[abac.resource_id]
-            local contexts = {}
-            for i, j in pairs(abac.contexts) do
-                contexts[i] = {
-                    type = j["name"],
-                    id = m[j["id"]]
-                }
-            end
+    -- Rule was found
+    if m then
+      local resource_id = m[abac.resource_id]
+      local contexts = {}
+      for i, j in pairs(abac.contexts) do
+        contexts[i] = {
+          type = j["name"],
+          id = m[j["id"]]
+        }
+      end
 
-            local request = {
-                consumer = {
-                    user_id = user_id,
-                    client_id = details.client_id,
-                    mis_client_id = mis_client_id
-                },
-                resource = {
-                    action = abac.action,
-                    type = abac.resource,
-                    id = resource_id
-                },
-                contexts = contexts
+      local request = {
+        consumer = {
+          user_id = user_id,
+          client_id = details.client_id,
+          mis_client_id = mis_client_id
+        },
+        resource = {
+          action = abac.action,
+          type = abac.resource,
+          id = resource_id
+        },
+        contexts = contexts
+      }
+
+      local httpc = http.new()
+      local res, err =
+        httpc:request_uri(
+          abac.endpoint,
+          {
+            method = "POST",
+            body = json.encode(request),
+            headers = {
+              accept = "application/json",
+              ["Content-Type"] = "application/json",
+              ["x-request-id"] = ngx.ctx.correlationid_header_value
             }
+          }
+        )
 
-            local httpc = http.new()
-            local res, err =
-                httpc:request_uri(
-                    abac.endpoint,
-                    {
-                        method = "POST",
-                        body = json.encode(request),
-                        headers = {
-                            accept = "application/json",
-                            ["Content-Type"] = "application/json",
-                            ["x-request-id"] = ngx.ctx.correlationid_header_value
-                        }
-                    }
-                )
+      httpc:close()
 
-            httpc:close()
-
-            if not res or res.status ~= 200 then
-                send_error(403, "Access denied")
-                return ngx.exit(200)
-            end
-
-            local response = json.decode(res.body)
-            local result = response.data.result
-
-            if result == false then
-                send_error(403, "Access denied")
-                return ngx.exit(200)
-            end
-
-        end
-    else
-        send_error(401, "Abac rule was not found")
+      if not res or res.status ~= 200 then
+        send_error(403, "Access denied")
         return ngx.exit(200)
+      end
+
+      local response = json.decode(res.body)
+      local result = response.data.result
+
+      if result == false then
+        send_error(403, "Access denied")
+        return ngx.exit(200)
+      end
+
     end
+  else
+    send_error(401, "Abac rule was not found")
+    return ngx.exit(200)
+  end
 end
 
 local function do_process_mis_only()
-    local api_key = ngx.req.get_headers()["api-key"]
-    if api_key ~= nil then
-        rate_limiting.verify(config, api_key)
-        local url = string.gsub(config.url_template, "{api_key}", api_key)
+  local api_key = ngx.req.get_headers()["api-key"]
+  if api_key ~= nil then
+    rate_limiting.verify(config, api_key)
+    local url = string.gsub(config.url_template, "{api_key}", api_key)
 
-        local verify_error_msg = "Invalid api key"
-        local res, err = verify_url(verify_error_msg)
-        local mis_client_id, details, scope, broker_scope = verify_details(res)
+    local verify_error_msg = "Invalid api key"
+    local res, err = verify_url(verify_error_msg)
+    local mis_client_id, details, scope, broker_scope = verify_details(res)
 
-        if scope == nil then
-            send_error(401, "Invalid api key")
-            return ngx.exit(200)
-        end
-
-        set_mis_client_id(scope, mis_client_id, details)
-
-        local rule = find_rule(config.rules)
-        if next(config.rules) ~= nil then
-            check_scopes(rule, scope, broker_scope)
-        end
-    else
-        send_error(401, "Api key is not set")
-        return ngx.exit(200)
+    if scope == nil then
+      send_error(401, "Invalid api key")
+      return ngx.exit(200)
     end
+
+    set_mis_client_id(scope, mis_client_id, details)
+
+    local rule = find_rule(config.rules)
+    if next(config.rules) ~= nil then
+      check_scopes(rule, scope, broker_scope)
+    end
+  else
+    send_error(401, "Api key is not set")
+    return ngx.exit(200)
+  end
 end
 
 local function do_process(authorization)
-    if authorization ~= nil then
-        local bearer = string.sub(authorization, 8)
-        rate_limiting.verify(config, bearer)
-        local url = string.gsub(config.url_template, "{access_token}", bearer)
+  if authorization ~= nil then
+    local bearer = string.sub(authorization, 8)
+    rate_limiting.verify(config, bearer)
+    local url = string.gsub(config.url_template, "{access_token}", bearer)
 
-        local verify_error_msg = "Invalid access token"
-        local res, err = verify_url(verify_error_msg)
-        local mis_client_id, details, scope, broker_scope = verify_details(res)
+    local verify_error_msg = "Invalid access token"
+    local res, err = verify_url(verify_error_msg)
+    local mis_client_id, details, scope, broker_scope = verify_details(res)
 
-        if user_id == nil or scope == nil then
-            send_error(401, "Invalid access token")
-            return ngx.exit(200)
-        end
-
-        ngx.req.set_header("x-consumer-id", user_id)
-        ngx.var.upstream_x_consumer_id = user_id
-        set_mis_client_id(scope, mis_client_id, details)
-
-        local rule = find_rule(config.rules)
-        if next(config.rules) ~= nil then
-            check_scopes(rule, scope, broker_scope)
-            check_abac(rule, user_id, mis_client_id, details)
-        end
-    else
-        send_error(401, "Authorization header is not set or doesn't contain Bearer token")
-        return ngx.exit(200)
+    if user_id == nil or scope == nil then
+      send_error(401, "Invalid access token")
+      return ngx.exit(200)
     end
+
+    ngx.req.set_header("x-consumer-id", user_id)
+    ngx.var.upstream_x_consumer_id = user_id
+    set_mis_client_id(scope, mis_client_id, details)
+
+    local rule = find_rule(config.rules)
+    if next(config.rules) ~= nil then
+      check_scopes(rule, scope, broker_scope)
+      check_abac(rule, user_id, mis_client_id, details)
+    end
+  else
+    send_error(401, "Authorization header is not set or doesn't contain Bearer token")
+    return ngx.exit(200)
+  end
 end
 
 return MithrilHandler
